6ccf2d9a39fff2941615ee322a42b0260ed232df
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-07-14 11:07:54 +0300

    vue create

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+npm run build
+
+git add -f ./dist
+git commit -m "build"
+git push --delete origin gh-pages
+git subtree --prefix dist push origin gh-pages
+git reset HEAD^ --soft
+rm -rf ./dist
+git reset -- ./dist
